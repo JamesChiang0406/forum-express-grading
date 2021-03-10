8ca7f8b8aa39d3d776d3176d3f1c7466b19450e7
James_C <a9356070@gmail.com>
2021-03-10 16:46:02 +0800

    DELETE /api/admin/categories/:id

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -32,13 +32,12 @@ let categoryController = {
   },
 
   deleteCategory: (req, res) => {
-    return Category.findByPk(req.params.id)
-      .then((category) => {
-        category.destroy()
-          .then((category) => {
-            res.redirect('/admin/categories')
-          })
-      })
+    categoryService.deleteCategory(req, res, (data) => {
+      if (data['status'] === 'success') {
+        req.flash('success_messages', data['message'])
+        return res.redirect('/admin/categories')
+      }
+    })
   }
 }
 
--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ const upload = multer({ dest: 'temp/' })
 
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController')
+const categoryService = require('../services/categoryService.js')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
@@ -15,5 +16,6 @@ router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 router.get('/admin/categories', categoryController.getCategories)
 router.post('/admin/categories', categoryController.postCategory)
 router.put('/admin/categories/:id', categoryController.putCategory)
+router.delete('/admin/categories/:id', categoryController.deleteCategory)
 
 module.exports = router
